e78b605e49c187ccc071460869b28266247e9c49
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-02-24 21:14:24 -0500

    Tema 5

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -2,8 +2,138 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
+    .titulo-principal.color-acento-botones
+      .titulo-principal__numero
+        span 5
+      h1 Acting y entretenimiento
 
+    div.tema3-div1.p-4.mb-0
+      p.mb-5 Cuando  se habla de acting, se referiere a la interpretación que los personajes animados darán en el proyecto audiovisual, puesto que, cada personaje tendrá unas características definidas que lo llevarán a comportarse y moverse de acuerdo con su expresión y estado de ánimo, imprimiendo características particulares en cada uno, ver figura 6.
+
+      .titulo-sexto.color-acento-contenido
+        h5 Figura 6
+        span Expresión de la emoción 
+
+      .row.justify-content-center
+        .col-lg-7
+          figure
+            img(src="@/assets/curso/tema5/img01.png")
+            figcaption Nota. Tomado de https://elfolder.com/las-10-mejores-peliculas-en-3d-de-2020el-bloq-creativo/
+
+    .tema5-div1.py-5.mb-5
+      .cajon.color-secundario.p-4.mb-5    
+        p.mb-0 Básicamente, es crear emociones y expresiones de estas características, a un objeto inerte y hacerlo parecer que tiene vida; por ende, sus movimientos, expresiones y decisiones, deben tener coherencia con sus peculiaridades para que sea creíble, mediante la expresión corporal, lo que se busca entonces es generar credibilidad e interés, ver figura 7.
+      
+      .titulo-sexto.color-acento-contenido
+        h5 Figura 7
+        span Expresión de emoción
+
+      .row.justify-content-center.mb-5
+        .col-lg-7
+          figure
+            img(src="@/assets/curso/tema5/img02.png")
+            figcaption Nota. Tomado de https://www.xataka.com/cine-y-tv/las-peliculas-de-animacion-tambien-tienen-vfx-y-asi-se-hacen-en-deep
+
+      .row.justify-content-center.mb-5
+        .col-lg-6
+          p.mb-4 Para dar credibilidad a los personajes, es necesario observar el trabajo realizado y entender si el actor se siente vivo, evaluar si las acciones de este, provienen de su interior, y son inherentes a él, así como las reacciones que presenta, son consistentes con lo que le rodea, según el estado de ánimo y personalidad y con el guión que representa.
+          p.mb-4 Se busca entonces hacer que el público empatice con el personaje, que genere sentimientos y conexión. Para ello, es necesario en ocasiones que no todo sea instintivo o reactivo; además, es necesario darle espacio en el acting del personaje, para que refleje pensamientos y análisis.
+          p.mb-0 El personaje debe reaccionar al entorno en que lo rodea de manera coherente, así como también a otros personajes y objetos, cada acción debe tener un porqué, y ese porqué debe ser razonable. En el siguiente video podrás consultar sobre este tema de interés:
+        .col-lg-6
+          figure.mb-0
+            .video
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)          
+
+      p.mb-0 <b>El estado de ánimo del personaje, juega un papel importante en estas variaciones</b>, según cómo se desarrollan las situaciones, él tendrá diferentes estados de ánimo los cuales deben ser reflejados mediante su comportamiento corporal. No es suficiente solo demostrar actitud y personalidad en los personajes creados; además, debe ser consistente, si un personaje es extrovertido, debe tenerse claridad si hay un cambio en el estado de ánimo, y que sea justificado, de lo contrario se perderá la congruencia y coherencia del personaje. En los videos que se encuentran a continuación, se podrá observar ejemplos del estado de ánimo del personaje, mediante diálogos y expresiones corporales:
+
+    .tema-1.mb-5
+      TabsB.color-acento-botones
+        .p-4.py-md-5(titulo="Camina Pesado" :icono="require('@/assets/curso/tema1/img04.svg')")          
+          figure.mb-0
+            .video
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+        .p-4.py-md-5(titulo="Camina Orgulloso" :icono="require('@/assets/curso/tema1/img04.svg')")          
+          figure.mb-0
+            .video
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+        .p-4.py-md-5(titulo="Enojado" :icono="require('@/assets/curso/tema1/img04.svg')")          
+          figure.mb-0
+            .video
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+
+    .tema-5.mb-5
+      .row.justify-content-around.align-items-center
+        .col-lg-10
+          .tarjeta.color-acento-contenido.p-3.py-5
+            .row.justify-content-center.align-items-center
+              .col.lg-7.ps-5
+                h3.mb-2 Estados de ánimo y expresiones corporales
+                p.mb-0 La expresión del estado del ánimo, puede realizarse mediante la expresión corporal algunos ejemplos relacionados con este tema, se puedes consultar en YouTube, un video explicativo sobre el mismo tema, el enlace es:             
+              .col-lg-3
+                .row.justify-content-center.align-items-center
+                  .col.lg-1.d-flex.justify-content-center
+                    a.boton.color-acento-boton(href="https://www.youtube.com/watch?v=kdfwmDnCsJg" target="_blank")
+                      span Ver Video
+                      i.fas.fa-video
+
+    p.mb-5 <b>Hay que tener varios puntos en cuenta para preparar el proyecto audiovisual:</b>
+
+    .tema5-div2.py-4.mb-5
+      .row.justify-content-center.align-items-center
+        .col-lg-10
+          ul.lista-ul--color.mb-0
+            li
+              i.fas.fa-check.fa-xs(style="color: #6BBCB1 !important")
+              | El contexto de la escena.
+            li 
+              i.fas.fa-check.fa-xs(style="color: #6BBCB1 !important")
+              | Cómo se mueve físicamente el personaje, que lo diferencia de los demás personajes.
+            li.mb-0
+              i.fas.fa-check.fa-xs(style="color: #6BBCB1 !important")
+              | Elegir el mejor ángulo para transmitir una emoción.
+
+    p.mb-5 <b>Se puede consultar el siguiente video, en donde se aprecia aún más el manejo de las expresiones en el estado de ánimo y la expresión corporal:</b>
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        figure.mb-0
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+    .tema5-div3.mb-5       
+      p.mb-5 Además de lo anterior, es necesario crear puntos de atención, ya que estos ayudan a que el personaje destaque acciones, y resalte emociones, como, por ejemplo: si el personaje en determinado caso realiza una acción y todo lo demás está detenido. Así también el posado del personaje ayuda a que las emociones que se busca transmitir luzcan bien en pantalla, por esto se recomienda utilizar solo las poses necesarias, no se debe crear poses demás, esto ocasionará que los movimientos no se vean reales, ver figura 8.
+      .titulo-sexto.color-acento-contenido
+        h5 Figura 8
+        span Posado
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-8
+          figure.mb-0
+            .video
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+      p.mb-4 Es importante simplificar las tomas, para que cada acción se detalle y la atención se centre en cada una de ellas, el personaje debe moverse si lo amerita, pero si no hay movimientos justificables debe evitarse, la idea no es moverse por moverse, sino moverse en función de alguna acción. En la medida en que se dé tiempo a que los personajes hagan una transición entre una emoción y otra, se dará mayor realismo y veracidad al momento.
+      p.mb-5 Igualmente es necesario darle estilo a los movimientos del personaje para que reflejen su personalidad, los haga destacar y que queden en la memoria del espectador, además de la recordación del personaje, posibilitará la identificación con este y sus acciones.<br><br>
+
+    .tema-5
+      .row.justify-content-around.align-items-center
+        .col-lg-10
+          .tarjeta.color-acento-contenido.p-3.py-5
+            .row.justify-content-center.align-items-center
+              .col.lg-7.ps-5
+                h3.mb-2 Características del movimiento en personajes animados
+                p.mb-0 En el siguiente video de YouTube, se podrá observar las características del movimiento en personajes animados
+              .col-lg-3
+                .row.justify-content-center.align-items-center
+                  .col.lg-1.d-flex.justify-content-center
+                    a.boton.color-acento-boton(href="https://www.youtube.com/watch?v=MYZtU_aMItw " target="_blank")
+                      span Ver Video
+                      i.fas.fa-video
+      
+    Separador
 </template>
 
 <script>
